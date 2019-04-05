af0ca5e2bbb6fa2a2b83f61d439e80fa2342e308
alinavo <alinahuynhvo@gmail.com>
2019-04-05 08:41:01 -0400

    message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import ClickyGame from './components/ClickyGame';
+import App from './App';
 import './index.css';
 
-ReactDOM.render(<ClickyGame />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
