45090696117988fcd639318842f3ad6f13f1cdf8
alinavo <alinahuynhvo@gmail.com>
2019-04-05 12:00:54 -0400

    changes to container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,20 +2,17 @@ import React from 'react';
 import './Container.css';
 import Character from '../Character';
 
-// main container for character
-
 const Container = props => (
-  // loops through each index in props.characters
+
   <div
     className={
       props.shake
         ? 'container d-flex flex-wrap justify-content-center shake'
         : 'container d-flex flex-wrap justify-content-center'
     }
-  > 
+  >
     {props.characters.map((a, i) => <Character name={a} key={i} clickEvent={props.clickEvent} />)}
   </div>
 );
-//click event for character
 
-export default Container; 
+export default Container;
